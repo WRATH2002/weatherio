07d9540639b3224e556f0c8bf49608571b179916
Himadri Purkait <purkaithimadricollege@gmail.com>
2023-09-27 19:59:04 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ import { LuWaves } from "react-icons/lu";
 import { MdVisibility } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
 import { BsThermometerHalf } from "react-icons/bs";
-import night from "./assets/img/night.jpg";
+import { BiSolidUpArrow } from "react-icons/bi";
+import { LuAlarmClock } from "react-icons/lu";
+import { BiSolidDownArrow } from "react-icons/bi";
+
+// ----------------
+import snow_morning from "./assets/img/snow_morning.jpg";
+import snow_afternoon from "./assets/img/snow_afternoon.jpg";
+import snow_night from "./assets/img/snow_night.jpg";
+import thunderstorm from "./assets/img/thunderstorm.jpg";
+
+import rain from "./assets/img/rain.jpg";
+import cloudy from "./assets/img/cloudy.jpg";
 import morning from "./assets/img/morning.jpg";
+import night from "./assets/img/night.jpg";
 import afternoon from "./assets/img/afternoon.jpg";
-import snoww from "./assets/img/snoww.jpg";
 
 function App() {
   const [hour, setHour] = useState("");
@@ -22,9 +33,13 @@ function App() {
   const [year, setYear] = useState("");
   const [data, setData] = useState([]);
   const [weatherType, setWeatherType] = useState([]);
+  const [subWeatherType, setSubWeatherType] = useState([]);
   const [search, setSearch] = useState("");
   const [searchCity, setSearchCity] = useState("");
   const [temperature, setTemperature] = useState([]);
+  const [tempmax, setTempmax] = useState([]);
+  const [tempmin, setTempmin] = useState([]);
+
   const [wind, setWind] = useState([]);
 
   useEffect(() => {
@@ -40,6 +55,9 @@ function App() {
     setData(json);
     setTemperature(json?.main?.temp);
     setWeatherType(json?.weather[0]?.main);
+    setSubWeatherType(json?.weather[0]?.description);
+    setTempmax(json?.main?.temp_max);
+    setTempmin(json?.main?.temp_min);
     var today = new Date();
     let date = today.getDate();
     let month = today.getMonth() + 1;
@@ -93,14 +111,42 @@ function App() {
             </button>
           </div>
           <div className="flex justify-center">
-            <div className="w-full h-[208px] bg- mt-[20px] mb-[10px] rounded-xl p-[14px] flex justify-start items-center flex-col drop-shadow-2xl">
-              {hour >= 6 && hour <= 13 ? (
-                temperature <= 15 ? (
+            <div
+              id="imcontainer"
+              className="w-full h-[208px] bg- mt-[20px] mb-[10px] rounded-xl p-[14px] flex justify-start items-center flex-col drop-shadow-2xl"
+              style={{ transition: ".4s" }}
+            >
+              {weatherType === "Rain" ? (
+                <>
+                  <img
+                    src={rain}
+                    className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
+                    // style={{ transition: ".4s" }}
+                  ></img>
+                </>
+              ) : weatherType === "Clouds" ? (
+                <>
+                  <img
+                    src={cloudy}
+                    className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
+                    // style={{ transition: ".4s" }}
+                  ></img>
+                </>
+              ) : weatherType === "Thunderstorm" ? (
+                <>
+                  <img
+                    src={thunderstorm}
+                    className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
+                    // style={{ transition: ".4s" }}
+                  ></img>
+                </>
+              ) : hour >= 6 && hour <= 13 ? (
+                temperature <= 20 ? (
                   <>
                     <img
-                      src={snoww}
+                      src={snow_morning}
                       className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                      style={{ transition: ".4s" }}
+                      // style={{ transition: ".4s" }}
                     ></img>
                   </>
                 ) : (
@@ -108,17 +154,17 @@ function App() {
                     <img
                       src={morning}
                       className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                      style={{ transition: ".4s" }}
+                      // style={{ transition: ".4s" }}
                     ></img>
                   </>
                 )
               ) : hour > 13 && hour <= 20 ? (
-                temperature <= 15 ? (
+                temperature <= 20 ? (
                   <>
                     <img
-                      src={snoww}
+                      src={snow_afternoon}
                       className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                      style={{ transition: ".4s" }}
+                      // style={{ transition: ".4s" }}
                     ></img>
                   </>
                 ) : (
@@ -126,16 +172,16 @@ function App() {
                     <img
                       src={afternoon}
                       className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                      style={{ transition: ".4s" }}
+                      // style={{ transition: ".4s" }}
                     ></img>
                   </>
                 )
-              ) : temperature <= 15 ? (
+              ) : temperature <= 20 ? (
                 <>
                   <img
-                    src={snoww}
+                    src={snow_night}
                     className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                    style={{ transition: ".4s" }}
+                    // style={{ transition: ".4s" }}
                   ></img>
                 </>
               ) : (
@@ -143,13 +189,13 @@ function App() {
                   <img
                     src={night}
                     className="w-full h-full fixed mt-[-14px] rounded-xl drop-shadow-2xl"
-                    style={{ transition: ".4s" }}
+                    // style={{ transition: ".4s" }}
                   ></img>
                 </>
               )}
-              <div className="w-full h-full fixed mt-[-14px] bg-[#00000078] rounded-xl "></div>
+              <div className="w-full h-full fixed mt-[-14px] bg-[#00000045] rounded-xl "></div>
               <div
-                className="flex  flex-row justify-between items-center w-full text-[white] "
+                className="flex  flex-row justify-between items-center w-full text-[white] select-none"
                 style={{ zIndex: "5" }}
               >
                 <div
@@ -203,7 +249,7 @@ function App() {
                     {year}
                   </span>
                 </div>
-                <div className="flex flex-col justify-end items-end">
+                <div className="flex flex-col justify-end items-end select-none">
                   <span className="font-bold font-[CustomFont] text-[15px]">
                     {data.length === 0 ? <>__ , __</> : <>{data?.name}</>}
                   </span>
@@ -218,24 +264,63 @@ function App() {
               </div>
 
               <div
-                className="text-[50px] font-semibold text-white drop-shadow-2xl mt-[18px] font-[CustomFont]"
+                className="text-[50px] font-semibold text-white drop-shadow-2xl mt-[18px] font-[CustomFont] flex flex-row justify-between w-full items-center select-none"
                 style={{ transition: ".4s", zIndex: "5" }}
               >
+                <span className="flex text-[13px] justify-center items-center">
+                  {Math.round(tempmax)}°C
+                  <BiSolidUpArrow className="text-[15px] mx-[7px]" />
+                </span>
                 {temperature === undefined ? (
                   <>00°C</>
                 ) : (
                   <>{Math.round(temperature)}°C</>
                 )}
+                <span className="flex text-[13px] justify-center items-center">
+                  <BiSolidDownArrow className="text-[15px] mx-[7px]" />
+                  {Math.round(tempmin)}°C
+                </span>
               </div>
               <span
-                className="font-semibold text-white mt-[-10px] text-[16px]"
+                className="font-semibold text-white mt-[-10px] text-[16px] select-none"
                 style={{ zIndex: "5" }}
               >
-                {weatherType === null ? <>__</> : <>{weatherType}</>}
+                {weatherType === null ? (
+                  <>__</>
+                ) : (
+                  <>
+                    {weatherType},{" "}
+                    <span className="font-normal ">{subWeatherType}</span>
+                  </>
+                )}
+              </span>
+              <span
+                className="w-full  text-white  text-[13px] select-none drop-shadow-2xl font-[CustomFont] flex justify-center items-center mt-[18px]"
+                style={{ zIndex: "5" }}
+              >
+                {hour >= 6 && hour <= 13 ? (
+                  <>
+                    <span className="flex justify-center items-center">
+                      <LuAlarmClock className="mr-[8px]" /> Morning
+                    </span>
+                  </>
+                ) : hour > 13 && hour <= 20 ? (
+                  <>
+                    <span className="flex justify-center items-center">
+                      <LuAlarmClock className="mr-[8px]" /> Afternoon
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <span className="flex justify-center items-center">
+                      <LuAlarmClock className="mr-[8px]" /> Night
+                    </span>
+                  </>
+                )}
               </span>
             </div>
           </div>
-          <div className="w-full flex justify-between items-center flex-wrap  drop-shadow-xl">
+          <div className="w-full flex justify-between items-center flex-wrap  drop-shadow-xl select-none">
             <div className="w-[120px] h-[70px] drop-shadow-2xl bg-[white] text-black rounded-xl  my-[10px] flex justify-center items-center flex-col">
               <div className="flex justify-start mb-[10px] items-start w-[90%]">
                 <LuWind className="text-[20px] mx-[10px]" />
